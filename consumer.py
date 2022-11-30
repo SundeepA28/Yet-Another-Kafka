b269fc330c43e00dffe008c0c365d195bf9ace6c
Spoorthi Kalkunte <72077100+legbing@users.noreply.github.com>
2022-11-30 15:34:31 +0530

    Modified consumer to accept argument --from_beginning

--- consumer.py
+++ consumer.py
@@ -4,13 +4,20 @@ import sys
 import os
 import socket
 from _thread import *
+import argparse
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect(("localhost", 3750))
 
-topic = sys.argv[1]
+#topic = sys.argv[1]
 list_of_partitions = []
 
+parser = argparse.ArgumentParser()
+parser.add_argument('topic', type=str)
+parser.add_argument('--from_beginning', action='store_true')
+args = parser.parse_args()
+
+topic = args.topic
 
 offset = 0
 
@@ -37,45 +44,57 @@ cons_brok = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cons_brok.connect(('localhost', list_of_partitions[0][1]))
 
 offset = int(server.recv(2048).decode()) + 1
-
+print(offset)
 
 for i in range(len(list_of_partitions)):
 
-	def consumerthread(path, f, flag):
+	def consumerthread(path, f, l):
 		global offset
-		print(path)
+		#print(path)
+		#print("\n")
 		while True:
 			if os.path.getsize(path) != 0:
 				
 				line = f.readline().decode().strip().split(":")
-				#print(line)
+				len_line = len(":".join(line))
 				if(len(line)>1):
 					if(int(line[0]) == offset):
 						print(line[1])
 						offset = offset + 1
 					else:
-						f.seek(0, os.SEEK_CUR)
+						if args.from_beginning:
+							f.seek(-1*(len_line+1), os.SEEK_CUR)
+						else:
+							f.seek(0, os.SEEK_CUR)
+						
 			
 	path = "broker"+str(list_of_partitions[0][1])+"/"+topic+"/"+str(i)+".log"
 	print(path)
 	f = open(path, "rb")
 	#f.seek(-1,2)
 	flag = 0
-	if os.path.getsize(path) != 0:
-		l = f.readlines()
-		f.seek(0)
-		if len(l) == 1:
+	l = []
+	if args.from_beginning:
+		if os.path.getsize(path) != 0:
 			f.seek(0)
+			offset = 0
+	else:		
+		if os.path.getsize(path) != 0:
+			l = f.readlines()
+			f.seek(0)
+			if len(l) == 1:
+				f.seek(0)
+			else:
+				f.seek(-2, os.SEEK_END)
+				while f.read(1) != b'\n':
+			    		f.seek(-2, os.SEEK_CUR)
 		else:
-			f.seek(-2, os.SEEK_END)
-			while f.read(1) != b'\n':
-		    		f.seek(-2, os.SEEK_CUR)
-	else:
-		f.seek(0)
-		flag = 1
+			f.seek(0)
+			flag = 1
 	
 		
-	start_new_thread(consumerthread,(path, f, flag))
+	start_new_thread(consumerthread,(path, f, l))
 	
-input("Press enter to exit")
+input("Press enter to exit\n")
+
 
