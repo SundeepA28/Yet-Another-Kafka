cc99d32cc8f122e53dc1d46d826eb3aa30932569
thrupthi2002 <96109514+thrupthi2002@users.noreply.github.com>
2022-11-30 13:43:54 +0530

    initial commit of zookeeper.py

---
+++ zookeeper-2.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+
+# Working behind zookeper
+
+import sys
+import os
+import socket
+from _thread import *
+from time import sleep
+import pickle
+import traceback
+
+list_of_brokers = {}
+list_of_producers = []
+list_of_consumers = []
+topics = {}
+
+metalog = open("meta.log", "a")
+
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+	
+	def brokerthread(conn, addr):
+		while True:
+			msg = conn.recvmsg(2048)[0]
+			node, topic, partition, msg, offset = msg.decode().split("\n")
+			metalog = open("meta.log", "a")
+			metalog.write(":".join([topic, partition, offset]))
+			metalog.write("\n")
+			metalog.close()
+	
+	port = 3750
+	s1.bind(('localhost', 3750))
+	s1.listen(100)
+	print("Zookeeper listening")
+
+	i = 0
+	while i<3:
+		conn, addr = s1.accept()
+		print (addr[0] + " connected at " + str(addr[1]))
+		broker_port = int(conn.recv(2048).decode())
+		print("Broker port: ", broker_port)
+		list_of_brokers[broker_port] = conn
+		i+=1
+		start_new_thread(brokerthread, (conn, addr))
+		
+	
+	try:
+		with open('metadata.pkl', 'rb') as f:
+			topics = pickle.load(f)
+	except:
+		topics = {}
+		
+	def get_broker(partition, topic):
+		for x in topics[topic]:
+			if(x[0] == partition):
+				return x[1]
+
+	p = 0
+	rep_factor = 2
+	def clientthread(conn, addr,list_of_clients, topic):
+		
+		
+		# start publishing data
+		# brokers need to create log files for the partitions
+		#conn.send(topics[topic].encode())
+		print(topics[topic])
+		for i in topics[topic]:
+			conn.sendmsg([str(i[0]).encode(), "\n".encode(), str(i[1]).encode(), "\n".encode()])
+			sleep(1)
+			#conn.send(str(i[1]).encode())
+		'''	
+		while True:
+			try:
+				message = conn.recv(2048).decode()
+				partition = message[0] % partitions
+				port = get_broker(partition, topic)
+				conn.send(port.encode())
+			except:
+				continue	
+		'''
+	
+	def consumerthread(conn, addr,list_of_clients, topic):
+		
+		
+		# start publishing data
+		# brokers need to create log files for the partitions
+		#conn.send(topics[topic].encode())
+		print(topics[topic])
+		for i in topics[topic]:
+			conn.sendmsg([str(i[0]).encode(), "\n".encode(), str(i[1]).encode(), "\n".encode()])
+			sleep(1)
+			#conn.send(str(i[1]).encode())
+			
+		
+	def stdinthread():
+		global p
+		for line in sys.stdin:
+			message = line.strip()
+			print("zookeeper: ", port, message)
+			if message == "":
+				break
+			else:
+				# If the message is for the creation of a topic
+				if message.startswith("create"):
+					# Get the topic name
+					topic = message.split(" ")[1]
+					topics[topic] = []
+					partitions = message.split(" ")[2]
+					for k in range(int(partitions)):
+						topics[topic].append((k, list(list_of_brokers.keys())[p]))
+						p = (p+1) % len(list_of_brokers)
+						
+					with open('metadata.pkl', 'wb') as f:
+						pickle.dump(topics, f)
+					for brokers in list_of_brokers.values():
+
+						brokers.send(message.encode())
+					
+
+	start_new_thread(stdinthread,())
+
+	while True:	
+		
+		
+		conn, addr = s1.accept()
+		print (addr[0] + " connected")
+		try:
+			
+			msg = conn.recv(2048)
+			msg, topic = msg.decode().split("\n")
+			print("Message: ", msg)
+			if(msg == "producer"):
+				list_of_producers.append(conn)
+				
+				start_new_thread(clientthread,(conn,addr,list_of_producers, topic))
+				
+			elif (msg == "consumer"):
+				list_of_consumers.append(conn)
+				print(topics[topic])
+				for i in topics[topic]:
+					conn.sendmsg([str(i[0]).encode(), "\n".encode(), str(i[1]).encode(), "\n".encode()])
+					sleep(1)
+				cons_f = open("meta.log", "rb")
+				cons_f.seek(-2, os.SEEK_END)
+				while cons_f.read(1) != b'\n':
+				    	cons_f.seek(-2, os.SEEK_CUR)
+				
+				line = cons_f.readline().decode().strip().split(":")
+				print(line[2])
+				conn.send(line[2].encode())
+			
+		except Exception as e:
+			print(e, "Error in message receving")
+	
+except Exception as e:
+	print(e)
+	print(traceback.format_exc())
+	with open('metadata.pkl', 'wb') as f:
+		pickle.dump(topics, f)
+	s1.close()
+    
